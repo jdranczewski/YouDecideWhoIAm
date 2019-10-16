c0fc7988d0038e3905ced0a82ea7e45ef5086781
Jakub Dranczewski <domdlapsa@gmail.com>
2019-10-16 20:18:13 +0100

    Very basic submission form that adds text to the db
    Time stamps and a displayed flag set automatically in sql

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>SimpleWebStats - data</title>
+        <meta name="description" content="You decide who I am">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <form action="submit.php" method="post">
+            <input type="text" name="text" maxlength="24">
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
---
+++ submit.php
@@ -0,0 +1,44 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "YouDecideWhoIAm";
+
+// Establish a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare the statement and insert value into the db
+if ($_POST["text"] !== "") {
+    $sql = "INSERT INTO submissions (text) VALUES (?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $_POST["text"]);
+    $stmt->execute();
+    if ($stmt->error) {
+        echo "Error: ".$stmt->error;
+    } else {
+        header( "refresh:5;url=index.php" );
+    }
+    $stmt->close();
+}
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>SimpleWebStats - data</title>
+        <meta name="description" content="YDWIA - Thanks for your submission!">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        Thank you for submitting <?php echo $_POST["text"]?>!<br><br>
+        You will be redirected to the home page in a few seconds, where you can make a new submission!
+    </body>
+</html>
+
+<?php $conn->close(); ?>
