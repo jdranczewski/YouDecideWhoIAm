c3d79c77221ea7c018c380a961aa18c8f6a5e1d0
Jakub Dranczewski <domdlapsa@gmail.com>
2019-10-16 21:48:43 +0100

    Description, more styling, and a footer

--- server/index.php
+++ server/index.php
@@ -10,10 +10,26 @@
     </head>
     <body>
         <div id="main">
+            <h1>You decide who I am</h1>
+            Hi, my name is <b>Jakub Dranczewski</b> (pronounced <i>Yakoob</i>, but make the <i>oo</i> quite short). For the purposes of this Halloween party you are the one to decide who I came as!
+            <br><br>
+            Put anything into the text field below and it will be added to a pool of virtual 'costumes'. Every so often, my lanyard badge will select one of these randomly and <b>display it as my ID tag</b>.
+            <br><br>
+            It can be anything, from famous people, through references I probably won't get, to abstract concepts, adjectives, and other whatnots I could potentially be. <b>Get creative!</b>
+            <br><br>
+            I'll get a smartwatch notification every time a new submission comes through to my badge. If (for whatever contrived reason) you'd like to be notified too, <a href="https://t.me/YouDecideWhoIAm" target="_blank" title="The YDWIA Telegram channel">you can</a>!
+            <h2>TL;DR: Put in a thing you want me to be below. My badge will randomly display it.</h2>
             <form action="submit.php" method="post">
-                <input type="text" name="text" maxlength="24" id="text-input">
+                <input type="text" name="text" maxlength="24" id="text-input" placeholder="Type here...">
                 <input type="submit" value="Submit" id="submit">
             </form>
+        </div>
+        <div id="footer">
+            <a href="https://www.facebook.com/jakub.dranczewski" target="_blank">My Facebook</a> •
+            <a href="https://github.com/jdranczewski/YouDecideWhoIAm" target="_blank">Code on GitHub</a>
+            <div id="past">
+                Tonight I've been: TODO
+            </div>
         </div>
     </body>
 </html>
