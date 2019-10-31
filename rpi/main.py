ccfee6101d4fa61aee69f020ecc0d7aa55568ab5
Jakub Dranczewski <domdlapsa@gmail.com>
2019-10-31 00:35:24 +0000

    Get and show a message

--- rpi/main.py
+++ rpi/main.py
@@ -2,10 +2,19 @@
 # https://learn.adafruit.com/drive-a-16x2-lcd-directly-with-a-raspberry-pi/python-code
 
 from get_ip import ip
+from time import sleep
+from requests import get
+
 import board
 import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
 
+def get_message():
+    r = get("https://jdranczewski.cba.pl/YouDecideWhoIAm/get.php")
+    if r.status_code == 200:
+        return r.text
+    else:
+        return "request.get encountered an issue\n5"
 
 def main():
     # Set the pins being used
@@ -20,8 +29,27 @@ def main():
     lcd = characterlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5,
                                           lcd_d6, lcd_d7, 16, 2)
 
-    # Show the IP address
+    # Show the IP address and blink cursor
+    lcd.clear()
     lcd.message = ip()
+    lcd.blink = True
+    sleep(1)
+
+    while True:
+        pass
+        # Get a message
+        payload = get_message().split("\n")
+        text = "\n".join(payload[:-1])
+        try:
+            timeout = int(payload[-1])
+        except ValueError:
+            timeout = 5
+        # Display the message
+        lcd.clear()
+        lcd.message = text
+        # Allow 10 seconds to cancel
+        # Sleep for the designated amount of time
+
 
 if __name__ == "__main__":
     main()
